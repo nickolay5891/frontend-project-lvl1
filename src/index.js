cdf924f82dd4413911df3928344f25829b495fbc
nickolay5891 <nickolay5891@gmail.com>
2019-07-27 00:23:47 +0300

    refactoring indexs.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const game = (rulesGame, createQuestionAnswer) => {
+export default (rulesGame, createQuestionAnswer) => {
   console.log('Welcome to the Brain Games!');
   console.log(rulesGame);
   const userName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${userName}!`);
-  let n = 0;
-  while (n < 3) {
+  let NumberOfQuestions = 0;
+  while (NumberOfQuestions < 3) {
     const QuestionAnswer = createQuestionAnswer();
     const question = QuestionAnswer[0];
     const correctAnswer = QuestionAnswer[1];
@@ -16,13 +16,12 @@ const game = (rulesGame, createQuestionAnswer) => {
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
       console.log('Correct!');
-      n += 1;
+      NumberOfQuestions += 1;
     } else {
       console.log(`'${answer}' is wrong answer;(. Correct answer was '${correctAnswer}'`);
       console.log(`Let,s try again, ${userName}!`);
+      return;
     }
   }
   console.log(`\nCongratulation, ${userName}!`);
 };
-
-export default game;
