db225df5b27608c343a13f2f32ae68e731436816
nickolay5891 <nickolay5891@gmail.com>
2019-08-01 00:56:11 +0300

    correct prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,17 +1,20 @@
 import game, { getRandomNumber } from '..';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-const createQuestionAnswer = () => {
-  const primeNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-  const question = getRandomNumber(0, 100);
-  let prime = 0;
-  for (let i = 0; i < primeNumber.length; i += 1) {
-    if (primeNumber[i] === question) {
-      prime += 1;
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
   }
-  const correctAnswer = (prime === 1) ? 'yes' : 'no';
+  return true;
+};
+const createQuestionAnswer = () => {
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = (isPrime(question)) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 export default () => game(task, createQuestionAnswer);
