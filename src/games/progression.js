d6e54b247ebcf17cb9ed5adb5e7e8e418d3aba3c
nickolay5891 <nickolay5891@gmail.com>
2019-08-01 01:07:54 +0300

    correct progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,13 +5,13 @@ const createQuestionAnswer = () => {
   const firstNumberProgression = getRandomNumber(0, 10);
   const stepProgression = getRandomNumber(1, 10);
   const progressionLength = 10;
-  const progression = [firstNumberProgression];
-  for (let i = 1; i < progressionLength; i += 1) {
-    progression[i] = progression[i - 1] + stepProgression;
+  const progression = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression[i] = firstNumberProgression + stepProgression * i;
   }
-  const index = getRandomNumber(0, progressionLength - 1);
-  const correctAnswer = `${progression[index]}`;
-  progression.splice(index, 1, '..');
+  const hiddenNumberIndex = getRandomNumber(0, progressionLength - 1);
+  const correctAnswer = `${progression[hiddenNumberIndex]}`;
+  progression.splice(hiddenNumberIndex, 1, '..');
   const question = `${progression.join(' ')}`;
   return [question, correctAnswer];
 };
