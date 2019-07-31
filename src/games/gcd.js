9c9be5ba375fb0fb484e3abce838f87ca31a3e7f
nickolay5891 <nickolay5891@gmail.com>
2019-07-31 23:57:48 +0300

    correctgcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,16 +1,16 @@
 import game, { getRandomNumber } from '..';
 
 const task = 'Find the greatest common divisor of given numbers.';
+const searchGcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return searchGcd(b, a % b);
+};
 const createQuestionAnswer = () => {
   const RandomNumber1 = getRandomNumber(0, 100);
   const RandomNumber2 = getRandomNumber(0, 100);
   const question = `${RandomNumber1} ${RandomNumber2}`;
-  const searchGcd = (a, b) => {
-    if (b === 0) {
-      return a;
-    }
-    return searchGcd(b, a % b);
-  };
   const correctAnswer = `${searchGcd(RandomNumber1, RandomNumber2)}`;
   return [question, correctAnswer];
 };
