6aee187a044396eefd3c37fae5738f996ffa148c
nickolay5891 <nickolay5891@gmail.com>
2019-08-06 00:56:41 +0300

    use destructuring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const questionsCount = 3;
+
 export default (rulesGame, createQuestionAnswer) => {
   console.log('Welcome to the Brain Games!');
   console.log(rulesGame);
   const userName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${userName}!`);
-  const numberOfQuestions = 3;
-  for (let i = 0; i < numberOfQuestions; i += 1) {
-    const QuestionAnswer = createQuestionAnswer();
-    const question = QuestionAnswer[0];
-    const correctAnswer = QuestionAnswer[1];
+  for (let i = 0; i < questionsCount; i += 1) {
+    const [question, correctAnswer] = createQuestionAnswer();
     console.log(`\nQuestion: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
