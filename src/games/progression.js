33edf93c7ab945274ef1a680cd487a32dd98a26d
nickolay5891 <nickolay5891@gmail.com>
2019-08-06 00:23:49 +0300

    use toString

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,19 +1,20 @@
 import game from '..';
 import getRandomNumber from '../utils';
 
+const progressionLength = 10;
+
 const task = 'What number is missing in the progression?';
 const createQuestionAnswer = () => {
-  const firstNumberProgression = getRandomNumber(0, 10);
-  const stepProgression = getRandomNumber(1, 10);
-  const progressionLength = 10;
+  const firstElement = getRandomNumber(0, 10);
+  const step = getRandomNumber(1, 10);
   const progression = [];
   for (let i = 0; i < progressionLength; i += 1) {
-    progression[i] = firstNumberProgression + stepProgression * i;
+    progression[i] = firstElement + step * i;
   }
   const hiddenNumberIndex = getRandomNumber(0, progressionLength - 1);
-  const correctAnswer = `${progression[hiddenNumberIndex]}`;
+  const correctAnswer = progression[hiddenNumberIndex].toString();
   progression.splice(hiddenNumberIndex, 1, '..');
-  const question = `${progression.join(' ')}`;
+  const question = progression.join(' ');
   return [question, correctAnswer];
 };
 export default () => game(task, createQuestionAnswer);
